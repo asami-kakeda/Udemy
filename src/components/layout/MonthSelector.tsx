a945eba0b7cc276965fc913c24e328e5c93eaf23
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-07-11 10:00:47 +0900

    DatePickerを使用して日付を日本語化する

--- src/components/layout/MonthSelector.tsx
+++ src/components/layout/MonthSelector.tsx
@@ -3,17 +3,32 @@ import React from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
+import { ja } from "date-fns/locale";
 
 const MonthSelector = () => {
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
+    <LocalizationProvider
+      dateAdapter={AdapterDateFns}
+      adapterLocale={ja}
+      dateFormats={{ month: "yyyy年 MM月" }}
+    >
       <Box
         sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
       >
         <Button color={"error"} variant="contained">
           先月
         </Button>
-        <DatePicker sx={{ mx: 2 }} />
+        <DatePicker
+          label="年月を選択"
+          sx={{ mx: 2, background: "white" }}
+          views={["year", "month"]}
+          format="yyyy/MM"
+          slotProps={{
+            toolbar: {
+              toolbarFormat: "yyyy年MM月",
+            },
+          }}
+        />
         <Button color={"primary"} variant="contained">
           次月
         </Button>
