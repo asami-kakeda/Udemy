a4f154048ebbf8b762e3f058cd43853b25458c21
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-07-13 15:15:11 +0900

    仮データの円グラフを作成する

--- src/components/layout/CategoryChart.tsx
+++ src/components/layout/CategoryChart.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const CategoryChart = () => {
-  return <div>CategoryChart</div>;
+  const data = {
+    labels: ["Windows", "Mac", "Linux"],
+    datasets: [
+      {
+        data: [60, 30, 10],
+        backgroundColor: ["#4169e1", "#ff1493", "#FFCE56"],
+        hoverBackgroundColor: ["#36A2EB", "#FF6384", "#FFCE56"],
+        borderColor: ["transparent", "transparent", "transparent"],
+      },
+    ],
+  };
+
+  return <Pie data={data} />;
 };
 
 export default CategoryChart;
